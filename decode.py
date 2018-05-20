6c2378283a23e4d88c54a59fe9c87c83cbd4016b
joker353 <35500359+joker353@users.noreply.github.com>
2018-05-20 22:20:31 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -15,35 +15,26 @@ message = []
 decode = True
 
 
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-    n = int(bits, 2)
-    print(n)
-    return int2bytes(n).decode(encoding, errors)
-
-
 def int2bytes(i):
     hex_string = '%x' % i
     n = len(hex_string)
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
 
-while decode and len(pix) > 0:
+while len(pix) > 0:
     pos = pix[random.randint(0, len(pix)-1)]
     pix.remove(pos)
     cr = pos // img.shape[0]
     cc = pos - (pos // img.shape[0]) * img.shape[0]
-    print(img[cr, cc][0] & 0x01)
     buffer.append(img[cr, cc][0] & 0x01)
-    if len(buffer) == 7:
+    if len(buffer) == 8:
         buffer = ''.join(map(str, buffer))
-        print(buffer)
-        val = text_from_bits(buffer)
+        n = int(buffer, 2)
+        if n == 255:    #255 = 0xFF
+            break
+        val = int2bytes(n).decode(encoding='utf-8', errors='surrogatepass')
         buffer = []
-        print('val = ' + val )
-        if ord(val) == 127:
-            decode = False
-        else:
-            message.append(val)
+        message.append(val)
 
 message = ''.join(map(str, message))
 print(message)
